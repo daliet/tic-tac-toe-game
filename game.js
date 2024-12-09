f45e1fa4290505ec775852b4a3259d3b0266b524
daliet <95545630+daliet@users.noreply.github.com>
2024-12-09 19:45:42 +0000

    Add displayWinner function

--- game.js
+++ game.js
@@ -25,10 +25,38 @@ const gameBoard = (() =>{
                 if(box.textContent === '') { // Only allow marking if the box is empty
                     board[index] = currentPlayer.symbol; // Update the board with the current player's symbol at the correct index
                     box.textContent = currentPlayer.symbol; // Update the text of the box with the current player's symbol
+                    displayWinner();
                     switchPlayer();
                 }
             })
-        })}
+        })
+    }
+
+    function displayWinner(){
+
+        const winningCombinations = [[0, 1, 2],
+                                     [0, 3, 6],
+                                     [1, 4, 7],
+                                     [2, 5, 8],
+                                     [3, 4, 5],
+                                     [6, 7, 8],
+                                     [2, 4, 6],
+                                     [0, 4, 8]]
+    
+        for(let combination of winningCombinations){ // Loop through the winning combinations to check if any player has won
+            if(combination.every(index => board[index] === 'X')){ // Checks if all positions -that are specified in the combination array- of the board array have the value of 'X'
+                winner.textContent = `${player1.name} WINS`;
+            }else if(combination.every(index => board[index] === 'O')){
+                winner.textContent = `${player2.name} WINS`;
+            } 
+        }
+            if(board.every(item => item !== '')){ // Checks that every item in the board array is not an empty string (i.e the board is full)
+                winner.textContent = "IT'S A TIE";
+            }
+            
+    }
+
+
         return {board, addMark};
 })();
 
